e43e05e3b56c0250443fc49dee511bfc45d9a4c5
TermuxID <31366985+krisnaedunt@users.noreply.github.com>
2020-03-08 14:47:02 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,55 @@
+<?php
+
+error_reporting(0);
+include ("func.php");
+echo "\e            GOJEK VERSION 1.7.2            \n";
+echo "\e         Semoga Harimu Menyenangkan        \n";
+echo "\n";
+nope:
+echo "\e[?] Masukkan Nomor HP Anda (62) : ";
+$nope = trim(fgets(STDIN));
+$cek = cekno($nope);
+if ($cek == false)
+    {
+    echo "\e[x] Nomor Telah Terdaftar Cok\n";
+			goto nope;
+    }
+  else
+    {
+echo "\e[!] Siapkan OTPmu\n";
+sleep(5);
+$register = register('62'.$nope);
+if ($register == false)
+    {
+    echo "\e[x] Failed Get OTP!\n";
+    }
+  else
+    {
+    otp:
+    echo "\e[!] Masukkan Kode Verifikasi (OTP) : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[x] Kode Verifikasi Salah\n";
+        goto otp;
+        }
+      else
+        {
+		$h=fopen("newgojek.txt","a");
+		fwrite($h,json_encode(array('token' => $verif, 'voc' => 'gofood gak ada'))."\n");
+		fclose($h); 
+                echo "\e[!] Trying to redeem Voucher : GOFOOD022620A !\n";
+                sleep(3);
+            $claim = pengen($verif);
+            if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+            }else{
+                echo "\e[+] ".$claim."\n";
+            }
+    }
+    }
+    }
+
+
+?>
