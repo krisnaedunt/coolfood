53258e92c006e8d9173fc484b3626679ed65cd4f
TermuxID <31366985+krisnaedunt@users.noreply.github.com>
2020-03-15 23:21:16 +0700

    Update jakarta.php

--- lib/jakarta.php
+++ lib/jakarta.php
@@ -51,7 +51,7 @@ $nama = nama();
 					echo color("yellow",".");
 					sleep(1);
 				}
-				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDEPAY"}');
+				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HEPIORINS"}');
 				$message = fetch_value($code1,'"message":"','"');
 				if(strpos($code1, 'You can use this promo now...')){
 					echo "\n".color("green","[+] Message: ".$message);
@@ -66,7 +66,7 @@ $nama = nama();
 						sleep(1);
 					}
 					sleep(3);
-					$boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+					$boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HEPIAKANG"}');
 					$messageboba10 = fetch_value($boba10,'"message":"','"');
 					if(strpos($boba10, 'You can use this promo now...')){
 						echo "\n".color("green","[+] Message: ".$messageboba10);
@@ -82,7 +82,7 @@ $nama = nama();
 						sleep(1);
 					}
 					sleep(3);
-					$goride = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOCARPAY"}');
+					$goride = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HEPIBOXNCO"}');
 					$message1 = fetch_value($goride,'"message":"','"');
 					echo "\n".color("green","[+] Message: ".$message1);
 							
@@ -93,7 +93,7 @@ $nama = nama();
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"BELANJAINAJA"}');
+					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HEPIUPNORMAL"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","[+] Message: ".$message2);
 					sleep(3);
